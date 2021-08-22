d8a9dc83e1c253083cd23978540c8a0df4492bd4
Seth Stadick <sstadick@gmail.com>
2021-08-22 17:45:55 -0600

    Add an impl

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
-# crabz
+# 🦀 crabz
+
+Like pigz, but rust.
+
+## Synopsis
+
+This is currently a proof of concept CLI tool using the [`gzp`](https://github.com/sstadick/gzp/) crate.
+
+## Benchmarks
+
+This are very anecdotal. Data [here](https://archive.ics.uci.edu/ml/machine-learning-databases/00347/all_train.csv.gz).
+
+Compiled with `cargo --release` and no tricks.
+
+```bash
+cat data.csv | /usr/bin/time crabz -c 3 > crabby.gz
+79.34user 4.86system 0:06.52elapsed 1291%CPU (0avgtext+0avgdata 29868maxresident)k
+0inputs+3632904outputs (0major+68221minor)pagefaults 0swaps
+```
+
+```bash
+cat data.csv | /usr/bin/time pigz -3 > crabby.gz
+120.65user 12.90system 0:11.36elapsed 1174%CPU (0avgtext+0avgdata 22904maxresident)k
+0inputs+3763984outputs (0major+5352minor)pagefaults 0swaps
+```
+
+```bash
+/usr/bin/time ./target/release/crabz -c 3 -o ./data.csv.gz data.csv
+78.56user 3.72system 0:05.99elapsed 1373%CPU (0avgtext+0avgdata 30276maxresident)k
+0inputs+3632904outputs (0major+64057minor)pagefaults 0swaps
+```
+
+```bash
+/usr/bin/time pigz -3 data.csv 
+121.20user 9.77system 0:08.01elapsed 1634%CPU (0avgtext+0avgdata 23240maxresident)k
+0inputs+3763976outputs (0major+5360minor)pagefaults 0swaps
+```
+
+`crabz` seems to be about 20-30% faster than pigz.
+
+This should be apples to apples with the same buffers, number of threads, and compression used.
+The round trip md5sums tie out.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,124 @@
-fn main() {
-    println!("Hello, world!");
+use color_eyre::Report;
+use gzp::deflate::Gzip;
+use gzp::parz::{Compression, ParZ};
+use std::fs::File;
+use std::io::{self, BufReader, BufWriter, Read, Write};
+use std::path::PathBuf;
+use std::process::exit;
+use structopt::{clap::AppSettings::ColoredHelp, StructOpt};
+use tracing::info;
+use tracing_subscriber::EnvFilter;
+
+/// Get a bufferd input reader from stdin or a file
+fn get_input(path: Option<PathBuf>) -> Result<Box<dyn Read>, Report> {
+    let reader: Box<dyn Read> = match path {
+        Some(path) => {
+            if path.as_os_str() == "-" {
+                Box::new(BufReader::new(io::stdin()))
+            } else {
+                Box::new(BufReader::new(File::open(path)?))
+            }
+        }
+        None => Box::new(BufReader::new(io::stdin())),
+    };
+    Ok(reader)
+}
+
+/// Get a buffered output writer from stdout or a file
+fn get_output(path: Option<PathBuf>) -> Result<Box<dyn Write + Send + 'static>, Report> {
+    let writer: Box<dyn Write + Send + 'static> = match path {
+        Some(path) => {
+            if path.as_os_str() == "-" {
+                Box::new(BufWriter::new(io::stdout()))
+            } else {
+                Box::new(BufWriter::new(File::create(path)?))
+            }
+        }
+        None => Box::new(BufWriter::new(io::stdout())),
+    };
+    Ok(writer)
+}
+
+/// Check if err is a broken pipe.
+#[inline]
+fn is_broken_pipe(err: &Report) -> bool {
+    if let Some(io_err) = err.root_cause().downcast_ref::<io::Error>() {
+        if io_err.kind() == io::ErrorKind::BrokenPipe {
+            return true;
+        }
+    }
+    false
+}
+
+/// A small program to do clean up delimited data.
+///
+/// For each field in each record this will do the following:
+///
+/// 1. Remove the delimiter from inside any quoted fields
+/// 2. Remove the terminator from inside any quoted fields
+/// 3. Fix any non-UTF8 encodings
+#[derive(StructOpt, Debug)]
+#[structopt(name = "cleanse", author, global_setting(ColoredHelp))]
+struct Opts {
+    /// Output path to write to, "-" to write to stdout
+    #[structopt(short, long)]
+    output: Option<PathBuf>,
+
+    /// Input file to read from, "-" to read from stdin
+    #[structopt(name = "FILE", parse(from_os_str))]
+    file: Option<PathBuf>,
+
+    /// Compression level
+    #[structopt(short, long, default_value = "3")]
+    compression_level: u32,
+}
+
+fn main() -> Result<(), Report> {
+    let opts = setup()?;
+    if opts.compression_level > 9 {
+        return Err(Report::msg("Invalid compression level"));
+    }
+
+    if let Err(err) = run(
+        get_input(opts.file)?,
+        get_output(opts.output)?,
+        opts.compression_level,
+    ) {
+        if is_broken_pipe(&err) {
+            exit(0)
+        }
+        return Err(err);
+    }
+    Ok(())
+}
+
+/// Run the program, returning any found errors
+fn run<R, W>(mut input: R, output: W, compression_level: u32) -> Result<(), Report>
+where
+    R: Read,
+    W: Write + Send + 'static,
+{
+    let mut parz: ParZ<Gzip> = ParZ::builder(output)
+        .compression_level(Compression::new(compression_level))
+        .build();
+    io::copy(&mut input, &mut parz)?;
+    parz.finish()?;
+    Ok(())
+}
+/// Parse args and set up logging / tracing
+fn setup() -> Result<Opts, Report> {
+    if std::env::var("RUST_LIB_BACKTRACE").is_err() {
+        std::env::set_var("RUST_LIB_BACKTRACE", "1");
+    }
+    color_eyre::install()?;
+
+    if std::env::var("RUST_LOG").is_err() {
+        std::env::set_var("RUST_LOG", "info");
+    }
+    tracing_subscriber::fmt::fmt()
+        .with_env_filter(EnvFilter::from_default_env())
+        .with_writer(std::io::stderr)
+        .init();
+
+    Ok(Opts::from_args())
 }
